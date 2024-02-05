3c5593ce615550d630ccc34cf76ad7fa110cdc73
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-02-05 13:36:49 +0000

    refactor: Moving the PhotoViewer into imageSelector

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import { PhotoViewer } from "./components/photo_viewer/photo_viewer";
 import { ImageSelector } from "./components/image_selector/image_selector";
 import "./App.css";
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <div>
       <h1>React Photo Viewer</h1>
-      <PhotoViewer src={"https://picsum.photos/id/600/1600/900.jpg"} />
       <ImageSelector />
     </div>
   );
