5467f4295af28f1289bd2b499db66278da47e000
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-02-05 13:34:18 +0000

    feat: Implementing useState hook to display the selected image in the PhotoViewer

--- src/components/image_selector/image_selector.tsx
+++ src/components/image_selector/image_selector.tsx
@@ -1,16 +1,26 @@
-import React from "react"; // import React (to provide access to TSX)
+import React, { useState } from "react"; // import React (to provide access to TSX)
 import "./image_selector.css";
 import { getImageUrls } from "./get_image_urls";
+import { PhotoViewer } from "../photo_viewer/photo_viewer";
 
 export function ImageSelector() {
   const urls = getImageUrls();
+  const [selectedImg, setSelectedImg] = useState<string>();
+
+  // const handleClick=(event:React.MouseEvent<HTMLImageElement>)=>{
+  //     console.log(event.target)
+  //     // setSelectImg()
+  // }
   return (
-    <div className="thumbnails-container">
-      {urls.map((image) => (
-        <div className="thumbnails">
-          <img src={image} />
-        </div>
-      ))}
+    <div>
+      {selectedImg && <PhotoViewer src={selectedImg}/>}
+      <div className="thumbnails-container">
+        {urls.map((image) => (
+          <div className="thumbnails">
+            <img src={image} onClick={() => setSelectedImg(image)} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
