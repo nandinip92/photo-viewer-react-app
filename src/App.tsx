f77607f6420cb83143fc2af427c6d362d1506bdc
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-02-05 14:26:11 +0000

    refactor: Lifting the selectedImage useState uo i.e., onto app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { useState } from "react";
+import { PhotoViewer } from "./components/photo_viewer/photo_viewer";
 import { ImageSelector } from "./components/image_selector/image_selector";
 import "./App.css";
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState<string>();
   return (
     <div>
       <h1>React Photo Viewer</h1>
-      <ImageSelector />
+      {selectedImg && <PhotoViewer src={selectedImg}/>}
+      <ImageSelector onImageSelect={setSelectedImg}/>
     </div>
   );
 }
--- src/components/image_selector/image_selector.tsx
+++ src/components/image_selector/image_selector.tsx
@@ -1,26 +1,25 @@
 import React, { useState } from "react"; // import React (to provide access to TSX)
 import "./image_selector.css";
 import { getImageUrls } from "./get_image_urls";
-import { PhotoViewer } from "../photo_viewer/photo_viewer";
 
-export function ImageSelector() {
+interface ImageSelectorProps{
+  onImageSelect:(image:string)=>void;
+}
+export function ImageSelector({onImageSelect}:ImageSelectorProps) {
   const urls = getImageUrls();
-  const [selectedImg, setSelectedImg] = useState<string>();
+  //const [selectedImg, setSelectedImg] = useState<string>();
 
   // const handleClick=(event:React.MouseEvent<HTMLImageElement>)=>{
   //     console.log(event.target)
   //     // setSelectImg()
   // }
   return (
-    <div>
-      {selectedImg && <PhotoViewer src={selectedImg}/>}
-      <div className="thumbnails-container">
-        {urls.map((image) => (
-          <div className="thumbnails">
-            <img src={image} onClick={() => setSelectedImg(image)} />
-          </div>
-        ))}
-      </div>
+    <div className="thumbnails-container">
+      {urls.map((image) => (
+        <div className="thumbnails">
+          <img src={image} onClick={() => onImageSelect(image)} />
+        </div>
+      ))}
     </div>
   );
 }
