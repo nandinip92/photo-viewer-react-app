4ed70d7795ac43805b0f9b0fcaad4fd70727776f
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-05-09 06:13:35 +0100

    feat: scrolling to the top of the page when an image is selected

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { PhotoViewer } from "./components/photo_viewer/photo_viewer";
 import { ImageSelector } from "./components/image_selector/image_selector";
 import "./App.css";
 
 function App() {
   const [selectedImg, setSelectedImg] = useState<string>("");
+  useEffect(() => window.scrollTo({ top: 0, behavior: "smooth" }), [selectedImg]);
   return (
     <div>
       <h1>React Photo Viewer</h1>
